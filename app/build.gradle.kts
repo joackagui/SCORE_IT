d7f6e36f8ff9e51857eab493dff433bfc43179c2
DiegoOrellana <diegoorellanaggch13@gmail.com>
2024-12-09 09:14:34 -0400

    Pantalla Login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
+    //implementation("com.google.code.gson:gson2.11.0") // son las librerias necesarias para json
     implementation(libs.androidx.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
