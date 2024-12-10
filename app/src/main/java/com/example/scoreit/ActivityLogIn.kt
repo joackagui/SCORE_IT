27aecbc948a1c988811d647739a01e7f16851280
Joaquin Aguilera <joaquinaguilera1@upb.edu>
2024-12-10 01:05:13 -0400

    Inicio de la app en Log In, mejora del activity de Log In y cambio de pantalla de 'Log In' a 'Menu Principal'

---
+++ app/src/main/java/com/example/scoreit/ActivityLogIn.kt
@@ -0,0 +1,28 @@
+package com.example.scoreit
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.scoreit.adapters.RecyclerCampeonatosCreados.RecyclerCampeonatosCreados
+import com.example.scoreit.databinding.ActivityLogInBinding
+import com.example.scoreit.databinding.ActivityMenuPrincipalBinding
+
+class ActivityLogIn : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLogInBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLogInBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.botonLogin.setOnClickListener {
+            val activityMenuPrincipal = Intent(this, ActivityMenuPrincipal::class.java)
+            startActivity(activityMenuPrincipal)
+        }
+    }
+}
