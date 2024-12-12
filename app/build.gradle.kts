53f96e1730ecffa44622ac0c1ad1f13f8a499251
Joaquin Aguilera <joaquinaguilera1@upb.edu>
2024-12-11 20:04:22 -0400

    json y firebase incluidos

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id ("com.google.gms.google-services")
 }
 
 android {
@@ -44,8 +45,11 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
-    //implementation("com.google.code.gson:gson2.11.0") // son las librerias necesarias para json
     implementation(libs.androidx.constraintlayout)
+    implementation("com.google.code.gson:gson:2.11.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation(platform("com.google.firebase:firebase-bom:33.7.0"))
+    implementation("com.google.firebase:firebase-auth")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
